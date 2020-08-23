722d26df44b95b6fc539f085345b6e6fd3123ce5
Mahfujur Rahman <mrtbd88@gmail.com>
2020-08-23 12:52:08 -0400

    cl;ass path done

---
+++ SF2020-feature-SF-75/src/prec/FindCurrentDirectory.java
@@ -0,0 +1,20 @@
+package prec;
+
+public class FindCurrentDirectory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println ("MY PROJECT LOCATION AT: ==>> "+System.getProperty("user.dir"));
+			
+			//System.getProperty("user.dir") = C:\Users\kEYA\desktop\eclipse\SF2020
+			
+			String myTotalAmount = "$120";
+			// get rid of $
+			int inum = Integer.parseInt(myTotalAmount);
+			System.out.println ("int value is :" + inum);
+			
+	
+	}
+
+}
