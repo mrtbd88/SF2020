959ee5040407b140f2bc76af95694379eb1e4c40
Mahfujur Rahman <mrtbd88@gmail.com>
2020-08-26 18:28:01 -0400

    done

--- SF2020-feature-SF-75/src/base/Config.java
+++ SF2020-feature-SF-75/src/base/Config.java
@@ -17,18 +17,25 @@ public class Config extends WDFunctions {
 		getCurrentTime();
 	}
 
-	@Parameters("browser")
+//	@Parameters("browser")
+//	@BeforeMethod
+//	public void beforeEachTestMethod(String browser){
+//		if (browser.equalsIgnoreCase("ch")){
+//			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
+//			driver = new ChromeDriver(); 
+//			System.out.println (" chrome driver open ++++++++++++++++ ");
+//		} else if (browser.equalsIgnoreCase("ff")){
+//			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/drivers/geckodriver.exe");
+//			driver = new FirefoxDriver(); 
+//			System.out.println (" Firefox driver open ++++++++++++++++ ");
+	
 	@BeforeMethod
-	public void beforeEachTestMethod(String browser){
-		if (browser.equalsIgnoreCase("ch")){
-			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
-			driver = new ChromeDriver(); 
-			System.out.println (" chrome driver open ++++++++++++++++ ");
-		} else if (browser.equalsIgnoreCase("ff")){
-			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/drivers/geckodriver.exe");
-			driver = new FirefoxDriver(); 
-			System.out.println (" Firefox driver open ++++++++++++++++ ");
-		}
+	public void beforeEachTestMethod(){
+		
+		System.setProperty("webdriver.chrome.driver", (System.getProperty("user.dir")+"/drivers/chromedriver.exe"));
+	//System.setProperty("webdriver.chrome.driver", "C:\\Users\\Mahfuj Tuhin\\eclipse-workspace\\NN07\\drivers\\chromedriver.exe");
+		
+		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(45, TimeUnit.SECONDS);
